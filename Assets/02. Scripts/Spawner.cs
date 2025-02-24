facb21f4b3b46cb93cb1ae6230752b94a5aaa352
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-24 17:20:24 +0900

    Feat: 장애물, 아이템 Debug모드 추가
    Spawner 스크립트에
    장애물을 설치하기 편리하게
    디버그 모드 추가

--- Assets/02. Scripts/Spawner.cs
+++ Assets/02. Scripts/Spawner.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // 빈 오브젝트의 위치들
-    [SerializeField] private GameObject Obstacle;
+    // 빈 오브젝트의 위치
+    [SerializeField] private GameObject obstacles;
+    [SerializeField] private GameObject[] items;
+
+    [SerializeField] private bool showTopObstacleDebug;
+    [SerializeField] private bool showBottomObstacleDebug;
+    [SerializeField] private bool showTopItemDebug;
+    [SerializeField] private bool showBottomItemDebug;
 
     [Header("Obstacle Info")]
-    [SerializeField] private float TopSpawnY;
-    [SerializeField] private float BottomSpawnY;
+    [SerializeField] private float topSpawnY;
+    [SerializeField] private float bottomSpawnY;
 
     [Space]
     [SerializeField] private float topScaleY;
     [SerializeField] private float bottomScaleY;
+
+    [Header("Item Percentage")]
+    [SerializeField][Range(1, 10f)] private int percentage; // 아이템이 나올 확률
+
+    [Header("ItemSpawn Info")]
+    [SerializeField] private Color debugTopColor;
+    [SerializeField] private Color debugBottomColor;
+    [SerializeField] private float topSpawnItemY;
+    [SerializeField] private float bottomSpawnItemY;
 
     SpawnHandler spawnHandler;
 
@@ -25,8 +40,8 @@ public class Spawner : MonoBehaviour
     private void Awake()
     {
         Vector2 pos = transform.position;
-        TopSpawnPosition = pos + new Vector2(0, TopSpawnY);
-        BottomSpawnPosition = pos + new Vector2(0, BottomSpawnY);
+        TopSpawnPosition = pos + new Vector2(0, topSpawnY);
+        BottomSpawnPosition = pos + new Vector2(0, bottomSpawnY);
     }
 
     private void Start()
@@ -39,33 +54,71 @@ public class Spawner : MonoBehaviour
     public void RandomSpawnObstacle()
     {
         isBottom = (Random.Range(0, 2) == 0);
-        GameObject go = Instantiate(Obstacle);
+        GameObject go = Instantiate(obstacles);
         go.transform.parent = transform;
 
         if(isBottom)
         {
             go.transform.localPosition += (Vector3)BottomSpawnPosition;
             go.transform.localScale = new Vector3(transform.localScale.x, bottomScaleY);
+            RandomItemCreate(go.transform, BottomSpawnPosition + new Vector2(0, bottomScaleY + bottomSpawnItemY));
         }
         else
         {
             go.transform.localPosition += (Vector3)TopSpawnPosition;
             go.transform.localRotation = Quaternion.Euler(0, 0, 180f);
             go.transform.localScale = new Vector3(transform.localScale.x, topScaleY);
+            RandomItemCreate(go.transform, TopSpawnPosition + new Vector2(0, topScaleY + topSpawnItemY));
+        }
+
+
+    }
+
+    private void RandomItemCreate(Transform parant, Vector2 pos)
+    {
+        if (items.Length <= 0)
+            return;
+
+        if(Random.Range(0, 11) <= percentage)
+        {
+            int index = Random.Range(0, items.Length);
+            GameObject item = Instantiate(items[index]);
+            item.transform.localPosition = pos;
         }
     }
 
     private void OnDrawGizmos()
     {
         Vector2 pos = transform.position;
-        Vector2 TopDrawPosition = pos + new Vector2(0, TopSpawnY);
-        Vector2 BottomDrawPosition = pos + new Vector2(0, BottomSpawnY);
+        Vector2 topDrawPosition = pos + new Vector2(0, topSpawnY);
+        Vector2 bottomDrawPosition = pos + new Vector2(0, bottomSpawnY);
 
         Color origin = Gizmos.color;
-        Gizmos.color = Color.green;
 
-        Gizmos.DrawWireCube(TopDrawPosition, Vector3.one);
-        Gizmos.DrawWireCube(BottomDrawPosition, Vector3.one);
+        Gizmos.color = Color.green;
+        if(showTopObstacleDebug)
+        {
+            Vector2 debugPos = topDrawPosition - new Vector2(0, topScaleY / 2) / 2f;
+            Gizmos.DrawWireCube(debugPos, new Vector2(1, topScaleY / 2));
+        }
+
+        if (showBottomObstacleDebug)
+        {
+            Vector2 debugPos = bottomDrawPosition + new Vector2(0, bottomScaleY / 2) / 2f;
+            Gizmos.DrawWireCube(debugPos, new Vector2(1, bottomScaleY/ 2));
+        }
+
+        if(showTopItemDebug)
+        {
+            Gizmos.color = debugTopColor;
+            Gizmos.DrawWireCube(topDrawPosition + new Vector2(0, topScaleY + topSpawnItemY), Vector3.one);
+        }
+
+        if(showBottomItemDebug)
+        {
+            Gizmos.color = debugBottomColor;
+            Gizmos.DrawWireCube(bottomDrawPosition + new Vector2(0, bottomScaleY + bottomSpawnItemY), Vector3.one);
+        }
 
         Gizmos.color = origin;
     }
