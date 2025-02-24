6b21eb67af43879c5f61b1ca389e3ef14e686c85
Aephok <codeparadoxist@gmail.com>
2025-02-24 12:10:10 +0900

    [Feat] 속도 추가

--- Assets/02. Scripts/Level.cs
+++ Assets/02. Scripts/Level.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Level : MonoBehaviour
@@ -9,10 +10,26 @@ public class Level : MonoBehaviour
     public int obstacleCount = 0;
     public Vector3 ObstacleLastPosition = Vector3.zero;
 
+    public float bgSpeed = 1f;
+    public float bgTime =0f;
+    public void Update()
+    {
+
+        if ((bgTime >= 5)) // bgTime이 5이상이면
+        {
+            bgSpeed += 0.5f; // bground speed 0.5  상승
+            bgTime = 0;
+        }
+        
+        transform.position += Vector3.right * bgSpeed * Time.deltaTime;
+        bgTime += 1f *Time.deltaTime;
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("BackGround"))
+       
+        if (collision.CompareTag("Background"))
         {
             float widthOfBgObject = ((BoxCollider2D)collision).size.x; // 배경화면 가로 길이
             Vector3 pos = collision.transform.position; // 현재 위치
@@ -21,6 +38,12 @@ public class Level : MonoBehaviour
             return;
         }
 
+    }
+
+    public void Spawner()
+    {
+        GameObject[] TopObstacle;
+        GameObject[] BottomObstacle;
     }
 }
  
