f7fe46a74784c6b7566f3b58f5301966742b06c3
GoingRight <choibin1008@naver.com>
2025-02-25 15:01:00 +0900

    Chore : Character 클래스에서 Item 효과 적용

--- Assets/02. Scripts/Character.cs
+++ Assets/02. Scripts/Character.cs
@@ -188,6 +188,9 @@ public class Character : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Item"))
+        {
+
+        }
     }
 }
