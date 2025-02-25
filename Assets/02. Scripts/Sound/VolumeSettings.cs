52a3a0345b4cb841e0c252bbcb7402c302ddff75
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-25 17:57:22 +0900

    Feat: MasterUI, SFXUI 슬라이드 추가
    마스터 볼륨 설정과 사운드이펙트 슬라이드 추가

--- Assets/02. Scripts/Sound/VolumeSettings.cs
+++ Assets/02. Scripts/Sound/VolumeSettings.cs
@@ -5,11 +5,23 @@ using UnityEngine.UI;
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
-    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider masterSlider, musicSlider, sfxSlider;
+
+    public void SetMasterVolume()
+    {
+        float volume = masterSlider.value;
+        myMixer.SetFloat("master", Mathf.Log10(volume) * 20);
+    }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
         myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        myMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
     }
 }
