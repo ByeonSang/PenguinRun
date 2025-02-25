2908b5b4dc9d903575cab72c7750c9f24d9a083b
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-25 20:41:01 +0900

    Feat: Audio저장 및 사운드 조절 가능 추가
    PlayerPrefs 활용해서
    저장했습니다.

--- Assets/02. Scripts/Sound/VolumeSettings.cs
+++ Assets/02. Scripts/Sound/VolumeSettings.cs
@@ -7,21 +7,33 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider masterSlider, musicSlider, sfxSlider;
 
+    private void Start()
+    {
+        AudioManager audio = AudioManager.Instance;
+
+        masterSlider.value = audio.MasterValue;
+        musicSlider.value = audio.MusicValue;
+        sfxSlider.value = audio.SFXValue;
+    }
+
     public void SetMasterVolume()
     {
         float volume = masterSlider.value;
         myMixer.SetFloat("master", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", volume);
     }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
         myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
     }
 
     public void SetSFXVolume()
     {
         float volume = sfxSlider.value;
         myMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
     }
 }
