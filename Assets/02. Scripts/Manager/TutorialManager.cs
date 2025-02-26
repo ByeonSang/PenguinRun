bb82ec63ea0d5fcd3ff2498cf23a41da09c03c8b
Xenawn <codeparadoxist@gmail.com>
2025-02-26 09:30:32 +0900

    Feat: 튜토리얼 흐름 + 점프기능
     튜토리얼 흐름과 점프기능 추가

--- Assets/02. Scripts/Manager/TutorialManager.cs
+++ Assets/02. Scripts/Manager/TutorialManager.cs
@@ -4,7 +4,6 @@ using TMPro;
 using UnityEngine;
 using System.Threading;
 using System;
-using UnityEditor.VersionControl;
 public class TutorialManager : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -12,60 +11,43 @@ public class TutorialManager : MonoBehaviour
     private bool isTutorialActive = true; // 튜토리얼 진행 중 여부
     public GameObject tutorialPanel; // 텍스트 박스 UI 오브젝트
 
-    string[] str = { "안녕! 펭귄런에 온 걸 환영해! ", "간단한 펭귄런 조작법을 배워볼거야" ,"스페이스바를 눌러, 점프키를 배워보자"};
-    string[] slideStr = { "you can slide", "Press shift" };
+    string[] jumpStr = { "안녕! 펭귄런에 온 걸 환영해! ", "간단한 펭귄런 조작법을 배워볼거야", "스페이스바를 눌러, 점프키를 배워보자" };
+    string[] slideStr = { "이제 슬라이딩을 배워볼거야", "왼쪽 쉬프트키를 눌러서 슬라이딩을 해보자" };
     bool isTextBox = false;
     void Start()
     {
         StartCoroutine(WriteText());
     }
 
-    void Update()
-    {
-      
-    }
+
     IEnumerator WriteText()
     {
+        tutorialPanel.SetActive(true);
+        Time.timeScale = 0f; // 게임 멈추기
+        foreach (string message in jumpStr)
+        {
+            coachingText.text = message;
+            yield return new WaitForSecondsRealtime(2f); // 게임 멈춰도 시간 흐르게
+        }
+
+        tutorialPanel.SetActive(false); // 동작동안 판넬 제거
+        Time.timeScale = 1f; // 게임 동작
+        yield return StartCoroutine(TJump()); // 점프 실행
+        coachingText.text = "잘했어";
+
+        yield return new WaitForSecondsRealtime(2f); // 텍스트 확인할때 까지 2초 기다리기
         Time.timeScale = 0f; // 게임 멈추기
 
-        foreach (string message in str)
-        {
-            coachingText.text = message;
-            yield return new WaitForSecondsRealtime(2f); // 게임 멈춰도 시간 흐르게
-        }
-
-        Time.timeScale = 1f;
-        
-        TJump();
-
-        foreach (string message in slideStr)
-        {
-            coachingText.text = message;
-            yield return new WaitForSecondsRealtime(2f); // 게임 멈춰도 시간 흐르게
-        }
-
-        TSlide();
     }
- 
-     IEnumerator TJump()
-    {
-        tutorialPanel.SetActive(false);
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space)); // 스페이스바 입력 대기
 
-        coachingText.text = "Perfect!";
+    IEnumerator TJump()
+    {
+
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+
         tutorialPanel.SetActive(true);
-
     }
 
-    public void TSlide()
-    {
 
-       
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-           
-            coachingText.text = "Perfect!";
-
-        }
-    }
+  
 }
